fc121df15c39b097085efe6022bfdadca1dc3bd7
Michał Kopeć <michal@nozomi.space>
2021-11-08 23:20:12 +0100

    mb/e6540: WIP (booting!)

--- src/mainboard/dell/latitude_e6540/bootblock.c
+++ src/mainboard/dell/latitude_e6540/bootblock.c
@@ -1,8 +1,12 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
-
+#include <device/pci_ops.h>
+#include <ec/dell/mec5055/mec5055.h>
 #include <southbridge/intel/lynxpoint/pch.h>
 
-/* FIXME: remove this if not needed */
 void mainboard_config_superio(void)
 {
+	/* port 0x910 and 0x911 is needed to initialize the EC */
+	pci_write_config32(PCH_LPC_DEV, LPC_GEN2_DEC, 0x007c0901);
+	mec5055_early_init();
+	pci_write_config32(PCH_LPC_DEV, LPC_GEN2_DEC, 0x005c0921);
 }
